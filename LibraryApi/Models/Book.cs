7dd0f147593c2eaba745bcbfbdef45ae82e6e2bd
Vitor Carrera <vitorcarreraya@gmail.com>
2024-02-07 22:26:32 -0300

    Criação das classes de Modelo

---
+++ LibraryApi/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace LibraryApi.Models
+{
+    [Table("Books")]
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        [StringLength(80)]
+        public string? Name { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string? Description { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Price { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string? UrlImage { get; set; }
+        public float Stock { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public int GenreId { get; set; }
+        [JsonIgnore]
+        public Genre? Genre { get; set; }
+    }
+}
---
+++ LibraryApi/Models/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryApi.Models
+{
+    [Table("Genres")]
+    public class Genre
+    {
+        [Key]
+        public int GenreId { get; set; }
+        [Required]
+        [StringLength(80)]
+        public string? Name { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string? UrlImage { get; set; }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
