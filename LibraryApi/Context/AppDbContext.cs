700795bcf7d675d5c4833b1019d4a7952e60f7e2
Vitor Carrera <vitorcarreraya@gmail.com>
2024-02-07 22:55:44 -0300

    Criando a classe de Contexto

---
+++ LibraryApi/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LibraryApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
